f2ae4d679484fc2357fd41ad18a57cf4bc3f8b41
Smn-20 <rusekezasp@gmail.com>
2023-06-17 18:28:29 +0200

    test

--- server.js
+++ server.js
@@ -100,7 +100,16 @@ app.post('/', (req, res) => {
 
     var array = req.query["myinput"][sessionId].split('*')
 
-    console.log(array)
+    if(req.body.command.newrequest[0]=="1"){
+        if(req.body.command.input[0]!=="124*313"){
+            response_ = transformCommand(req.body.command,"Koresha *124*313# ","B")
+    
+            res.set('Content-type','text/xml')
+            res.send(xml(response_,true));
+            res.end
+        }
+        else{
+            console.log(array)
     if(array.length === 2){
       response_ = transformCommand(req.body.command,"Murakaza neza kuri Social registry ^ Shyiramo nomero yawe y'indangamuntu","C")
       res.set('Content-type','text/xml')
@@ -411,6 +420,323 @@ app.post('/', (req, res) => {
 
       }
   }
+        }
+    }
+    else{
+        console.log(array)
+        if(array.length === 2){
+          response_ = transformCommand(req.body.command,"Murakaza neza kuri Social registry ^ Shyiramo nomero yawe y'indangamuntu","C")
+          res.set('Content-type','text/xml')
+          res.send(xml(response_,true));
+          res.end
+        }
+    
+        if (array.length === 3) {
+          const options = {
+              method: 'POST',
+              url: 'https://api-gateway.uat.minaloc.gov.rw/users/auth/login-ussd',
+              headers: {
+                  'Content-Type': 'application/json',
+                  // 'Authorization': `Bearer ${token}`,
+                  'identificationNumber': array[2],
+                  'phone': req.body.command.msisdn[0].slice(2)
+              }
+          };
+    
+          
+    
+          axios.request(options).then(function (response) {
+              console.log(response.data.status)
+              if (response.data.status == true) {
+    
+                 
+                  response_ = transformCommand(req.body.command,"Ikaze kuri Social Registry ^ 1) Amakuru y'urugo ^ 2) Kwimuka ^ 3) Kujuririra amakuru ^ ","C")
+    
+                  res.set('Content-type','text/xml')
+                  res.send(xml(response_,true));
+                  res.end
+              }
+              else {
+                  response_ = transformCommand(req.body.command,"Wanditse irangamuntu nabi","B")
+    
+                  res.set('Content-type','text/xml')
+                  res.send(xml(response_,true));
+                  res.end
+              }
+          }).catch((err)=>{
+                  response_ = transformCommand(req.body.command,"Wanditse irangamuntu nabi","B")
+    
+                  res.set('Content-type','text/xml')
+                  res.send(xml(response_,true));
+                  res.end
+          })
+    
+    
+        }
+    
+    
+    
+    
+        if (array.length === 4) {
+          if (array[3] == "1") {
+              response_ = transformCommand(req.body.command,"1) Amakuru yimbitse y’urugo ^ 2) Abagize urugo","C")
+    
+              res.set('Content-type','text/xml')
+              res.send(xml(response_,true));
+              res.end
+          }
+    
+          if (array[3] == "2") {
+              response_ = transformCommand(req.body.command,"Andika UPI yaho ushaka kwimukira ^","C")
+    
+              res.set('Content-type','text/xml')
+              res.send(xml(response_,true));
+              res.end
+          }
+    
+          if (array[3] == "3") {
+    
+              response_ = transformCommand(req.body.command,"Hitamo ^ 1) Abanyamuryango ^ 2) Ibikorwa ugenewe ^ 3) Ishusho rusange y'umutungo ^","C")
+    
+              res.set('Content-type','text/xml')
+              res.send(xml(response_,true));
+              res.end
+          }
+        }
+    
+        if (array.length === 5) {
+          if (array[3] == "1") {
+              if (array[4] == "1") {
+                  const options = {
+                      headers: {
+                          'Content-Type': 'application/json',
+                          'Authorization': `Bearer ${token}`,
+                          'documentNumber': array[2],
+                      }
+                  };
+    
+                  axios.get(`https://api-gateway.uat.minaloc.gov.rw/households/view/household/by-document-number`, options).then((resp) => {
+                      console.log(resp.data)
+                      response_ = transformCommand(req.body.command,`Irangamuntu y'umukuru w'urugo:${resp.data.response.householdHead.nationalId} ^ Umukuru w'urugo: ${resp.data.response.householdHead.firstName} ${resp.data.response.householdHead.lastName}  ^ Kode: ${resp.data.response.code} ^ Ingano : ${resp.data.response.size} ^ Porogarumu: ${resp.data.response.targetingProgram}`,"B")
+    
+                      res.set('Content-type','text/xml')
+                      res.send(xml(response_,true));
+                      res.end
+                  }).catch((error) => {
+                      console.log(error)
+                  })
+    
+              }
+              if (array[4] == "2") {
+                  const options = {
+                      headers: {
+                          'Content-Type': 'application/json',
+                          'Authorization': `Bearer ${token}`,
+                          'documentNumber': array[2],
+                      }
+                  };
+    
+                  axios.get(`https://api-gateway.uat.minaloc.gov.rw/households/view/household/by-document-number`, options).then((resp) => {
+                      console.log(resp.data.response)
+                      axios.get(`https://api-gateway.uat.minaloc.gov.rw/households/${resp.data.response.id}/members`, {
+                          headers: {
+                              'Content-Type': 'application/json',
+                              'Authorization': `Bearer ${token}`,
+                          }
+                      }).then((resp) => {
+                          console.log(resp.data.response.members)
+                          var members = "Abagize urugo ^";
+    
+                          if (resp.data.response.members.length > 0) {
+                              for (var i = 0; i < resp.data.response.members.length; i++) {
+                                members += `${i + 1}. ${resp.data.response.members[i].firstName} ${resp.data.response.members[i].lastName} ^`
+                              }
+                          }
+                          else {
+                            members += 'Ntabandi bagize urugo'
+                          }
+    
+    
+    
+                          response_ = transformCommand(req.body.command,members,"B")
+    
+                          res.set('Content-type','text/xml')
+                          res.send(xml(response_,true));
+                          res.end
+                      }).catch((error) => {
+                          console.log(error)
+                      })
+                  }).catch((error) => {
+                      console.log(error)
+                  })
+    
+              }
+          }
+          if (array[3] == "3") {
+              response_ = transformCommand(req.body.command,"Andika Impamvu ^","C")
+    
+              res.set('Content-type','text/xml')
+              res.send(xml(response_,true));
+              res.end
+    
+          }
+    
+    
+          if (array.length == 5 && array[3] == "2") {
+              const options = {
+                  headers: {
+                      'Content-Type': 'application/json',
+                      'Authorization': `Bearer ${token}`,
+                      'upi': array[4],
+                  }
+              };
+    
+              axios.get(`https://api-gateway.uat.minaloc.gov.rw/land/upi/details`, options).then((resp) => {
+                //   console.log(resp.data.response)
+                  const upiInfo = resp.data.response
+                    if (resp.data.status == true) {
+                      const villageCode = resp.data.response.parcelLocation.village.villageCode
+                      const _villageCode = villageCode.substring(0, 1) + villageCode.substring(2);
+                      const options__ = {
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${token}`,
+                            'documentNumber': array[2],
+                        }
+                    };
+    
+                          axios.get(`https://api-gateway.uat.minaloc.gov.rw/households/view/household/by-document-number`, options__).then((resp) => {
+                                console.log(resp.data)
+                              const options_ = {
+                                  method: 'POST',
+                                  url: 'https://api-gateway.uat.minaloc.gov.rw/households/transfer',
+                                  headers: {
+                                      'Content-Type': 'application/json',
+                                      'Authorization': `Bearer ${token}`,
+                                  },
+                                  data: JSON.stringify({
+                                      "householdId": resp.data.response.id,
+                                      "villageCode": _villageCode,
+                                      'upi': upiInfo.upi,
+                                      'latitude': upiInfo.centralCoordinate.lat,
+                                      'longitude': upiInfo.centralCoordinate.lon,
+                                      'userType': 'CITIZEN',
+                                      'event': "REQUEST",
+                                      'memberDocumentNumber': array[2],
+                                      'phoneNumber':req.body.command.msisdn[0].slice(2)
+                                  })
+                              };
+    
+                              console.log(JSON.stringify({
+                                "householdId": resp.data.response.id,
+                                "villageId": _villageCode,
+                                'upi': upiInfo.upi,
+                                'latitude': upiInfo.centralCoordinate.lat,
+                                'longitude': upiInfo.centralCoordinate.lon,
+                                'userType': 'CITIZEN',
+                                'event': "REQUEST",
+                                'memberDocumentNumber': array[2],
+                                'phoneNumber':req.body.command.msisdn[0].slice(2)
+                            }))
+    
+                              axios.request(options_).then(function (response) {
+                                  console.log(response.data.response)
+                                  if (response.data.status == true) {
+                                      var transMessage = "Ubasabe bwakiriwe ^";
+                                  }
+                                  else {
+                                    var transMessage = response.data.response;
+                                  }
+                                  response_ = transformCommand(req.body.command,transMessage,"B")
+    
+                                  res.set('Content-type','text/xml')
+                                  res.send(xml(response_,true));
+                                  res.end
+                              }).catch(function (error) {
+                                  console.error(error);
+                              });
+                          }).catch((error) => {
+                              console.log(error)
+                          })
+    
+    
+                    
+    
+                  }
+                  else {
+                    response_ = transformCommand(req.body.command,"Mwanditse UPI itariyo","B")
+    
+                    res.set('Content-type','text/xml')
+                    res.send(xml(response_,true));
+                    res.end
+                  }
+              }).catch((error) => {
+                  console.log(error)
+              })
+    
+          }
+    
+        }
+    
+    
+    
+        if (array.length === 6) {
+    
+          
+          if (array[3] == "3") {
+    
+              const options = {
+                  headers: {
+                      'Content-Type': 'application/json',
+                      'Authorization': `Bearer ${token}`,
+                      'documentNumber': array[2],
+                  }
+              };
+    
+              axios.get(`https://api-gateway.uat.minaloc.gov.rw/households/view/household/by-document-number`, options).then((resp) => {
+                  console.log(resp.data.response)
+                  const options_ = {
+                      method: 'POST',
+                      url: 'https://api-gateway.uat.minaloc.gov.rw/households/public/appeals',
+                      headers: {
+                          'Content-Type': 'application/json',
+                          'Authorization': `Bearer ${token}`,
+                      },
+                      data: JSON.stringify({
+                          "documentNumber": array[2],
+                          "option": array[4] == 1 ? "PERSONAL_INFO" : (array[4] == 2 ? "ENROLLED_PROGRAM" : "SOCIAL_ECONOMIC"),
+                          "description": array[5],
+                          "householdID": resp.data.response.id,
+                          'phoneNumber':req.body.command.msisdn[0].slice(2)
+                      })
+                  };
+    
+    
+                  axios.request(options_).then(function (response) {
+                      console.log(response.data)
+                      if (response.data.status == true) {
+                          var appealMessage = "Ubasabe bwakiriwe ^";
+                      }
+                      else {
+                        var appealMessage = "Appeal request failed ^";
+                      }
+                      response_ = transformCommand(req.body.command,appealMessage,"B")
+    
+                      res.set('Content-type','text/xml')
+                      res.send(xml(response_,true));
+                      res.end
+                  }).catch(function (error) {
+                      console.error(error);
+                  });
+              }).catch((error) => {
+                  console.log(error)
+              })
+    
+          }
+      }
+    }
+
+    
 
 })
 
