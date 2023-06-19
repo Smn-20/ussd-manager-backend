3e7b441d6b027b03790b7ae33fe5702f95119236
Smn-20 <rusekezasp@gmail.com>
2023-06-19 15:05:16 +0200

    bug fixing

--- server.js
+++ server.js
@@ -524,11 +524,12 @@ app.post('/', (req, res) => {
 
                     axios.get(`https://api-gateway.uat.minaloc.gov.rw/households/view/household/by-document-number`, options).then((resp) => {
                         console.log(resp.data)
+                        var program = resp.data.response.targetingProgram
                         if (program == null) {
                             program = "Ntayo"
                         }
                         else {
-                            program
+                            program = program
                         }
                         response_ = transformCommand(req.body.command, `Irangamuntu y'umukuru w'urugo:${resp.data.response.householdHead.nationalId} ^ Umukuru w'urugo: ${resp.data.response.householdHead.firstName} ${resp.data.response.householdHead.lastName}  ^ Kode: ${resp.data.response.code} ^ Ingano : ${resp.data.response.size} ^ Porogarumu: ${program}`, "B")
 
