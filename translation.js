9d0ef3ce6ce01ab8a30d0e240d02d2e9082b8735
urbain12 <kortenyurbain@gmail.com>
2023-07-21 12:47:42 +0200

    translation

--- translation.js
+++ translation.js
@@ -1,16 +1,57 @@
 const translations = {
-    en:{
-        welcome:"Welcome to Social registry ^ Enter your national ID number",
-        initial:"1) Household Info ^ 2) Transfer ^ 3) Appeal ^ "
+    en: {
+        welcome: "Welcome to Social registry ^ Enter your national ID number",
+        initial: "1) Household Info ^ 2) Transfer ^ 3) Appeal ^ ",
+        wrongID: "You have entered the wrong ID",
+        enterupi: "Enter the UPI of your destination ^",
+        HHid: "Household head Identification",
+        names: "Household hea",
+        code: "Code",
+        size: "Number of members",
+        program: "Program",
+        none: "None",
+        writereason: "Write yoour reason ^ 0) Go back",
+        received: "request is accepted ^",
+        failed: "Request failed ^"
+
+
     },
-    fr:{
-        welcome:"Bienvenu au Social registry ^ Entrez votre numéro d'identification national",
-        initial:"1) Info sur le foyer ^ 2) Demande de transfert ^ 3) Réclamer ^ "
+    fr: {
+        welcome: "Bienvenu au Social registry ^ Entrez votre numéro d'identification national",
+        initial: "1) Info sur le foyer ^ 2) Demande de transfert ^ 3) Réclamer ^ ",
+        wrongID: "La carte d'identité est invalide",
+        enterupi: "Entrez l'UPI de l'emplacement où vous souhaitez vous déplacer ^",
+        HHid: "Identification du chef de foyer",
+        names: "Chef de foyer",
+        code: "Code",
+        size: "Taille des membres",
+        program: "Programmes",
+        none: "Aucun",
+        writereason: "Écrivez votre raison ^ 0) Retour",
+        received: "Demande acceptée ^",
+        failed: "Demande échoué ^"
+
+
+
     },
-    ki:{
-        welcome:"Murakaza neza kuri Social registry ^ Shyiramo nomero yawe y'indangamuntu",
-        initial:"1) Amakuru y'urugo ^ 2) Kwimuka ^ 3) Kujuririra amakuru ^ "
-    } 
+    ki: {
+        welcome: "Murakaza neza kuri Social registry ^ Shyiramo nomero yawe y'indangamuntu  ",
+        initial: "1) Amakuru y'urugo ^ 2) Kwimuka ^ 3) Kujuririra amakuru ^ ",
+        wrongID: "Wanditse irangamuntu nabi ",
+        enterupi: "Andika UPI yaho ushaka kwimukira ^",
+        HHid: "Irangamuntu y'umukuru w'urugo",
+        names: "Umukuru w'urugo",
+        code: "Kode",
+        size: "Umubare w'abagize urugo",
+        program: "Porogarumu",
+        none: "Ntayo",
+        writereason: "Andika Impamvu ^ 0) Gusubira Inyuma",
+        received: "Ubasabe bwakiriwe ^",
+        failed: "Gusaba ntibikunze^"
+
+
+
+    }
 }
 
 module.exports = translations;
